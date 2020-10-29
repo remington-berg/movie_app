0a02f8da0a052e594eb3fdae4796784d1664ea45
Remington <remingtonnberg@gmail.com>
2020-10-29 16:14:00 -0500

    add movies and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,15 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+# Character.create(name: 'Luke', movie: movies.first)
+
+movies = Movie.create({ title: "Happy Guilmore", year: 1996, plot: "A hockey player turns golfer." })
+
+require "ffaker"
+10.times do
+  actor = Actor.create({ first_name: FFaker::Name.first_name, last_name: FFaker::Name.last_name, known_for: FFaker::Movie.title })
+end
+
 actor = Actor.new({ first_name: "Benedict", last_name: "Cumberbatch", known_for: "Sherlock" })
 actor.save
