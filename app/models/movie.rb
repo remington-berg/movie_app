3dc15ec1d0c8df9ebfec88bccbe544f9115f1ef7
Remington <remingtonnberg@gmail.com>
2020-11-16 07:45:22 -0600

    adds movie genre connection table

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,4 +4,6 @@ class Movie < ApplicationRecord
   validates :year, numericality: { only_integer: true }
   validates :plot, length: { minimum: 10, maximum: 250 }
   validates :director, presence: true
+  has_many :movie_genres
+  has_many :genres, through: :movie_genres
 end
